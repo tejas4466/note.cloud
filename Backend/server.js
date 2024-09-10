db31ba0841577442f8d85a90de37e40d6a68acd0
tejas4466 <tejaspokale22@gmail.com>
2024-09-10 23:22:09 +0530

    cors error solving

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["note-cloud-frontend-njsatx74j-tejas-pokales-projects.vercel.app"],
+    origin: ["note-cloud-frontend-87va2xqy9-tejas-pokales-projects.vercel.app"],
     credentials: true,
   })
 );
