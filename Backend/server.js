a1abd6a7c5310f9e58e1b1cda6951aedc61061ba
tejas4466 <tejaspokale22@gmail.com>
2024-09-10 23:17:11 +0530

    cors error solved after deployment

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["note-cloud-frontend-njsatx74j-tejas-pokales-projects.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
