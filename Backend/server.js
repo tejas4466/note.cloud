149002ea999573107329e2258587d3731f26ded9
tejas4466 <tejaspokale22@gmail.com>
2024-09-10 23:27:17 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["note-cloud-frontend-87va2xqy9-tejas-pokales-projects.vercel.app"],
+    origin: ["https://note-cloud-frontend-alpha.vercel.app"],
     credentials: true,
   })
 );
