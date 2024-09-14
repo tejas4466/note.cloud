7e33521ffb450a453adc8a5bb5048064e7974223
tejas4466 <tejaspokale22@gmail.com>
2024-09-14 14:21:53 +0530

    updated constants.js

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = "https://note-cloud-green.vercel.app/";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://note-cloud-backend.vercel.app/";
