ffc916926efba98317b9f6055f13ea4ec7c8d8ad
tejas4466 <tejaspokale22@gmail.com>
2024-10-12 16:19:44 +0530

    pwa

---
+++ Frontend/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "notecloud-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  // Add other assets like CSS, JS files
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
